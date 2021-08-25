32a6a7f2eb42e6b5547f476b2a4cdbd1436840ed
kate gleeson <kate.gleeson@thoughtbot.com>
2021-08-25 18:06:15 +0100

    Add LanguagesGetter class
    - Add class which is initialised with a github username, makes a
      request to the GitHub API to get the user's repos, and returns an
      array of repo languages.

---
+++ app/models/languages_getter.rb
@@ -0,0 +1,23 @@
+class LanguagesGetter
+  def initialize(username)
+    @username = username
+  end
+
+  def languages
+    repos.map(&:language).compact
+  end
+
+  private
+
+  attr_reader :username
+
+  def repos
+    client.repos(username)
+  end
+
+  def client
+    Octokit::Client.new(
+      access_token: ENV['GITHUB_ACCESS_TOKEN']
+    )
+  end
+end
---
+++ spec/models/languages_getter_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+describe LanguagesGetter do
+  describe '#languages' do
+    it 'returns array of languages' do
+      username = 'jane-smith'
+      language_getter = described_class.new(username)
+
+      stub_request(
+        :get, "https://api.github.com/users/#{username}/repos"
+      ).to_return(
+        headers: { content_type: 'application/json' },
+        body: [
+          { language: 'Ruby' },
+          { language: 'Ruby' },
+          { language: 'Ruby' },
+          { language: 'Ruby' }
+        ].to_json
+      )
+
+      expect(language_getter.languages).to eq(['Ruby', 'Ruby', 'Ruby', 'Ruby'])
+    end
+  end
+end
